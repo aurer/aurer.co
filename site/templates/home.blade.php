74d54be288593409adf992bb64e0471182c20242
Phil Maurer <philmau@gmail.com>
2019-05-29 20:39:31 +0100

    Update homepage content

--- site/templates/home.blade.php
+++ site/templates/home.blade.php
@@ -1,15 +1,24 @@
 @extends('layouts/home')
 
 @section('main')
-	@include('partials/page-content')
-	@component('components/section', ['name' => 'resources'])
-		<div class="Items Items--resources">
-			@foreach (page('resources')->children()->visible()->limit(3) as $item)
-				<div class="Item">
-					<h3><a href="{{ $item->url() }}">{{ $item->title() }}</a></h3>
-					<p class="Item-description">{{ $item->summary() }}</p>
-			</div>		
-			@endforeach
-		</div>
-	@endcomponent
+	<h2>Projects</h2>
+	<div class="Items Items--projects">
+		@foreach (page('projects')->children()->visible()->limit(3) as $item)
+			<div class="Item">
+				<a class="Item-link" href="{{ $item->url() }}">{{ $item->title() }}</a>
+			</div>
+		@endforeach
+	</div>
+
+	<h2>Get in touch...</h2>
+	<div class="Items Items--networks">
+		@foreach ($site->networks()->toStructure() as $item)
+			<div class="Item">
+				<a class="Item-link" href="{{ $item->url() }}" target="_blank" rel="noopener">
+					<img src="/assets/build/gfx/icons/{{ $item->network() }}.svg" alt="{{ $item->network() }}" />
+					<span>{{ ucfirst($item->network()) }}</span>
+				</a>
+			</div>
+		@endforeach
+	</div>
 @endsection
