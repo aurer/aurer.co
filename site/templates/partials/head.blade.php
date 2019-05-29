55a24f89333a80d58265d73ec88d0d224816e9a1
Phil Maurer <philmau@gmail.com>
2019-05-29 21:57:11 +0100

    Add favicons

--- site/templates/partials/head.blade.php
+++ site/templates/partials/head.blade.php
@@ -1,9 +1,11 @@
 <meta charset="UTF-8">
 <meta name="description" content="{{ $site->description() }}">
-<meta name="keywords" content="{{ $site->keywords() }}" />
-<meta name="robots" content="index, follow" />
+<meta name="keywords" content="{{ $site->keywords() }}">
+<meta name="robots" content="index, follow">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>{{ $site->title() }}</title>
+<link rel="icon" href="/assets/build/gfx/favicon.png">
+<link rel="apple-touch-icon-precomposed" href="/assets/build/gfx/apple-touch-icon.png">
 @css('assets/build/main.css')
 @css('https://fonts.googleapis.com/css?family=Raleway:400,700&display=swap')
